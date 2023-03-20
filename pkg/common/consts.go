28cc3383086ecdbd62cbb49f54bb9feb333403b6
suutaku <johnsuu@cotnetwork.com>
2023-03-20 12:37:22 +0800

    change consts

--- pkg/common/consts.go
+++ pkg/common/consts.go
@@ -1,9 +1,9 @@
 package common
 
 const (
-	DefaultVCJsonLDContext       string = "https://www.w3.org/2018/credentials/v1"
-	DefaultBbsJsonLDContext      string = "https://w3id.org/security/bbs/v1"
-	DefaultStatusVCJsonLDContext string = "https://w3id.org/vc/status-list/2021/v1"
+	DefaultVCJsonLDContext       string = "https://ssis.cotnetwork.com/v1/schema/credentials/v1"
+	DefaultBbsJsonLDContext      string = "https://ssis.cotnetwork.com/v1/schema/bbs/v1.1"
+	DefaultStatusVCJsonLDContext string = "https://ssis.cotnetwork.com/v1/schema/status-list/2021/v1"
 	DefaultVCJsonLDContextTypeSC string = "StatusList2021Credential"
 	DefaultVCJsonLDContextTypeVC string = "VerifiableCredential"
 	DefaultVCJsonLDContextTypePR string = "VerifiablePresentation"
