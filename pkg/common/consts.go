bacaf20741bb71aa0e86c1dca0234a0ed6ce6776
suutaku <johnsuu@cotnetwork.com>
2023-03-31 19:15:45 +0800

    update version

--- pkg/common/consts.go
+++ pkg/common/consts.go
@@ -1,7 +1,7 @@
 package common
 
 const (
-	DefaultVCJsonLDContext       string = "https://ssis.cotnetwork.com/v1/schema/credentials/v1.1"
+	DefaultVCJsonLDContext       string = "https://ssis.cotnetwork.com/v1/schema/credentials/v2.0"
 	DefaultBbsJsonLDContext      string = "https://ssis.cotnetwork.com/v1/schema/bbs/v1.1"
 	DefaultStatusVCJsonLDContext string = "https://ssis.cotnetwork.com/v1/schema/status-list/v1.1"
 	DefaultVCJsonLDContextTypeSC string = "StatusList2021Credential"
